0ef55af8a5e9714aef4ab8c187519383eefca4e5
Otso Saarinen <saarinenotso@gmail.com>
2025-05-07 15:38:37 +0300

    add base page format

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { CountUp } from "countup.js";
 function App() {
     return (
         <>
-            <div className=""></div>
+            <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 bg-blue-400 text-neutral-800">
+                <h1 className="text-4xl">Aurajoen virtaus tänä vuonna:</h1>
+                <span className="text-8xl">
+                    123 m<sup>3</sup>/s
+                </span>
+            </div>
         </>
     );
 }
