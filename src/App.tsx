dc407c8f58d3c0b21ada755077ea2e422dde2eeb
Otso Saarinen <saarinenotso@gmail.com>
2025-05-08 11:23:44 +0300

    add initial values for yearly and monthly flow

--- src/App.tsx
+++ src/App.tsx
@@ -39,13 +39,32 @@ function App() {
 
     return (
         <>
-            <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 bg-blue-400 text-neutral-800">
-                <h1 className="text-4xl">
-                    Aurajoessa virranneen veden määrä tänään:
-                </h1>
-                <h2 className="text-8xl font-bold">
-                    <span id="waterFlow"></span> m<sup>3</sup>
-                </h2>
+            <div className="flex h-screen w-screen items-center justify-center bg-blue-400 text-neutral-800">
+                <div className="flex flex-col gap-5 rounded-md border-2 border-white bg-white p-2">
+                    <h1 className="text-3xl">
+                        Aurajoessa virranneen veden määrä
+                    </h1>
+                    <div>
+                        <div>
+                            <p>Tänä vuonna</p>
+                            <div className="text-4xl font-bold">
+                                <span id="">0</span> m<sup>3</sup>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Tässä kuussa</p>
+                            <div className="text-4xl font-bold">
+                                <span id="">0</span> m<sup>3</sup>
+                            </div>
+                        </div>
+                        <div>
+                            <p>Tänään</p>
+                            <div className="text-4xl font-bold">
+                                <span id="waterFlow">0</span> m<sup>3</sup>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
