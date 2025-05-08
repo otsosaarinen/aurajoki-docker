08d75495e83ae6276c0415662da6a80a9070bb20
Otso Saarinen <saarinenotso@gmail.com>
2025-05-08 11:28:23 +0300

    change styling

--- src/App.tsx
+++ src/App.tsx
@@ -39,27 +39,27 @@ function App() {
 
     return (
         <>
-            <div className="flex h-screen w-screen items-center justify-center bg-blue-400 text-neutral-800">
-                <div className="flex flex-col gap-5 rounded-md border-2 border-white bg-white p-2">
-                    <h1 className="text-3xl">
+            <div className="flex h-screen w-screen items-center justify-center text-neutral-800">
+                <div className="flex flex-col gap-5 rounded-md bg-neutral-200 p-2">
+                    <h1 className="text-4xl">
                         Aurajoessa virranneen veden määrä
                     </h1>
-                    <div>
+                    <div className="flex flex-col gap-3">
                         <div>
-                            <p>Tänä vuonna</p>
-                            <div className="text-4xl font-bold">
+                            <p className="italic">Tänä vuonna</p>
+                            <div className="text-4xl font-medium">
                                 <span id="">0</span> m<sup>3</sup>
                             </div>
                         </div>
                         <div>
-                            <p>Tässä kuussa</p>
-                            <div className="text-4xl font-bold">
+                            <p className="italic">Tässä kuussa</p>
+                            <div className="text-4xl font-medium">
                                 <span id="">0</span> m<sup>3</sup>
                             </div>
                         </div>
                         <div>
-                            <p>Tänään</p>
-                            <div className="text-4xl font-bold">
+                            <p className="italic">Tänään</p>
+                            <div className="text-4xl font-medium">
                                 <span id="waterFlow">0</span> m<sup>3</sup>
                             </div>
                         </div>
